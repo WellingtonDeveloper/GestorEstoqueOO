7137042a38b0ab6b4454b630a701074d9731f209
WellingtonDeveloper <wellington.developer.caldeira@gmail.com>
2022-06-18 19:52:44 -0300

    Add project files.

---
+++ GestorEstoqueOO/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace GestorEstoqueOO
+{
+    class Program
+    {
+        enum Menu {Listar = 1, Adicionar, Remover, Entrada, Saida, Sair}
+        static void Main(string[] args)
+        { 
+
+            bool escolheuSair = false;
+
+        while (escolheuSair == false){
+
+                Console.WriteLine("Sistema de Estoque");
+                Console.WriteLine("1 - Listar\n2 - Adicionar\n3 - Remover\n4 - Registrar Entrada\n5 - Registrar Saida\n6 - Sair");
+                string opStr = Console.ReadLine();
+                int opInt = int.Parse(opStr);
+
+
+                if(opInt > 0 && opInt < 7)
+                {
+                    Menu escolha = (Menu)opInt;
+
+                    switch (escolha)
+                    {
+                        case Menu.Listar:
+                            break;
+                        case Menu.Adicionar:
+                            break;
+                        case Menu.Remover:
+                            break;
+                        case Menu.Entrada:
+                            break;
+                        case Menu.Saida:
+                            break;
+                        case Menu.Sair:
+                            escolheuSair = true;
+                            break;
+                    }
+
+                }
+                else
+                {
+                    escolheuSair = true;
+                }
+
+
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
